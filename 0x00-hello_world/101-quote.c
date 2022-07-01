c6bc1b0ad5c66e7aa8e2bd7172daf7a1841efa0f
PaulSaferio <owakipaul@gmail.com>
2022-06-30 22:53:37 -0700

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,14 +8,9 @@
 *
 */
 
-int main(void)
-  
+int main(void)  
 {
-  
-  char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  
+  char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
   write(2, ch, 59);
-  
   return (1);
-  
 }
