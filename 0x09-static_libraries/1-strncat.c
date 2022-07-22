a02a87a2c9ef2d583525b4c1107a1aefe835f704
PaulSaferio <32652882+PaulSaferio@users.noreply.github.com>
2022-07-22 13:40:13 +0300

    1str

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
- */
+* _strncat - concatenates two strings,
+* @dest: destination.
+* @src: source.
+* @n: amount of bytes used from src.
+* Return: the pointer to dest.
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 int count = 0, count2 = 0;
@@ -21,5 +21,5 @@ break;
 count++;
 count2++;
 }
-	return (dest);
+return (dest);
 }
