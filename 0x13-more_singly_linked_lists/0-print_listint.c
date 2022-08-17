f3d6f0f35916e8fd83d136a0888dbc4e014b7842
PaulSaferio <32652882+PaulSaferio@users.noreply.github.com>
2022-08-17 15:46:59 +0300

    list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,12 @@
 */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+size_t nume = 0;
 while (h)
 {
 printf("%d\n", h->n);
-num++;
+nume++;
 h = h->next;
 }
-return (num);
+return (nume);
 }
