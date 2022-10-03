8ded5e1968bd931bd124a74b05df833a6c78f46b
Paul Saferio <32652882+PaulSaferio@users.noreply.github.com>
2022-10-03 17:22:40 +0300

    jamani

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,51 +1,26 @@
-#include "main.h"
-/**
- * add - addition
- * @a: number
- * @b: nuber
- * Return: result
- */
-int add(int a, int b)
+#include <stdio.h>
+int add(int a,int b)
 {
-      return (a + b);
+	return a + b;
 }
-/**
- * sub - subtraction
- * @a: number
- * @b: nuber
- * Return: result
- */
-int sub(int a, int b)
+
+int sub(int a,int b)
 {
-      return (a - b);
+        return a - b;
 }
-/**
- * mul - multiplication
- * @a: number
- * @b: nuber
- * Return: result
- */
-int mul(int a, int b)
+
+int mul(int a,int b)
 {
-      return (a * b);
+        return a * b;
 }
-/**
- * div - divission
- * @a: number
- * @b: nuber
- * Return: result
- */
-int div(int a, int b)
+
+
+int div(int a,int b)
 {
-	return (a / b);
+        return a / b;
 }
-/**
- * mod - modulus
- * @a: number
- * @b: nuber
- * Return: result
- */
-int mod(int a, int b)
+
+int mod(int a,int b)
 {
-      return (a % b);
+        return a % b;
 }
