4af28606162266b9858b048ab979bb063c4c2756
Paul Saferio <32652882+PaulSaferio@users.noreply.github.com>
2022-10-03 16:40:00 +0300

    wueeeh

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -28,7 +28,6 @@ int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
 int mod(int a, int b);
-double div(double a, double b);
-/*extern div_t div(int __numer, int __denom);*/
+extern div_t div(int __numer, int __denom);
 
 #endif /* MAIN_H */
